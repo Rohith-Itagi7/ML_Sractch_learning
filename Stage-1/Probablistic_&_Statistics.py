f9a9e838168e97daa01998c9cfc6820c920bda58
Rohith-Itagi7 <rohititagi2@gamil.com>
2026-03-03 01:03:09 +0530

    Initial-commit

--- Stage-1/Probablistic_&_Statistics.py
+++ Stage-1/Probablistic_&_Statistics.py
@@ -13,12 +13,14 @@ print(data_total)
 v1=np.mean(data)
 print(v1)
 
+# Manual varience
 squared=0
 for value in data:
     squared+=(value-v1)**2
     var_1=squared/len(data)
 print(var_1)
 
+#Using Numpy
 v2=np.var(data)
 print(v2)
 
@@ -28,8 +30,20 @@ std_manual = np.sqrt(variance)
 
 print("Manual Standard Deviation:", std_manual)
 
+#Normal Distribution
 import matplotlib.pyplot as plt
 import numpy as np
 
 data_1 = np.random.normal(0, 1, 1000)
 print(data_1)
+
+#Conditional Probalility
+study = 60
+pass_total = 50
+study_and_pass = 50
+
+p_pass_given_study = study_and_pass / study
+p_study_given_pass = study_and_pass / pass_total
+
+print(p_pass_given_study)
+print(p_study_given_pass)
